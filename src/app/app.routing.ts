035faf14ed7bfad7a1470db5aca901ed7db3ad2f
Ganesh Krishna R <ganeshkrishna98@gmail.com>
2023-12-05 23:16:00 +0530

    added lazy loading for components in app routing.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -25,19 +25,46 @@ const routes: Routes = [
     redirectTo: '/dashboard',
     pathMatch: 'full',
   },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: 'user-profile', component: UserProfileComponent },
-  { path: 'notifications', component: NotificationsComponent },
-  { path: 'create-document', component: CreateDocumentComponent },
-  { path: 'document-management', component: DocumentManagementComponent },
-  { path: 'admin-panel', component: AdminPanelComponent },
-  { path: 'access-logs', component: AccessLogsComponent },
-  { path: 'database-management', component: DatabaseManagementComponent },
-  { path: 'device-management', component: DeviceManagementComponent },
-  { path: 'notification-management', component: NotificationManagementComponent },
-  { path: 'storage-management', component: StorageManagementComponent },
-  { path: 'user-management', component: UserManagementComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', 
+  loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule), 
+  canActivate: [AuthGuard] 
+  },
+  { path: 'user-profile',
+  loadChildren: () => import('./features/user-profile/user-profile.module').then(m => m.UserProfileModule)
+  },
+  { path: 'notifications',
+  loadChildren: () => import('./features/notifications/notifications.module').then(m => m.NotificationsModule)
+  },
+  { path: 'create-document',
+   component: CreateDocumentComponent 
+  },
+  { path: 'document-management',
+   component: DocumentManagementComponent 
+  },
+  { path: 'admin-panel',
+  loadChildren: () => import('./features/admin-panel/admin-panel.module').then(m => m.AdminPanelModule)
+  },
+  { path: 'access-logs',
+  loadChildren: () => import('./features/admin-panel/management/access-logs/access-logs.module').then(m => m.AccessLogsModule)
+  },
+  { path: 'database-management',
+  loadChildren: () => import('./features/admin-panel/management/database-management/database-management.module').then(m => m.DatabaseManagementModule)
+  },
+  { path: 'device-management',
+  loadChildren: () => import('./features/admin-panel/management/device-management/device-management.module').then(m => m.DeviceManagementModule)
+  },
+  { path: 'notification-management',
+  loadChildren: () => import('./features/admin-panel/management/notification-management/notification-management.module').then(m => m.NotificationManagementModule)
+  },
+  { path: 'storage-management',
+  loadChildren: () => import('./features/admin-panel/management/storage-management/storage-management.module').then(m => m.StorageManagementModule)
+  },
+  { path: 'user-management',
+  loadChildren: () => import('./features/admin-panel/management/user-management/user-management.module').then(m => m.UserManagementModule)
+  },
+  { path: 'login',
+  loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule)
+  },
   {
     path: 'document',
     loadChildren: () => import('./features/document/document.module').then(m => m.DocumentModule)
